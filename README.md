0683c19ec6eb3c0d6ed220dc02f94f98f91838d2
Vince <vincent.groves@leanix.net>
2022-06-25 16:52:44 +0200

    with changed files

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple GitHub action to allow devs to have their markdown in the repo without ha
 ## Usage
 This action helps you automate your changes to your markdown and the subsequent upload to your readme.io project to automate dev workflows.
 
-### Example workflow
+### Example simple workflow
 ```yaml
 name: My Workflow
 on: [push, pull_request]
@@ -15,13 +15,52 @@ jobs:
     - uses: actions/checkout@master
     - name: Run action
 
-      uses: vg-leanix/docs-uploader@latest
+      uses: vg-leanix/docs-uploader@master
 
       with:
         docVersion: v1.0.0
         apiKey: foobar123
         categoryId: 12fff324
         settingsPath: docs.yml
+```
+### Example workflow with [changed files](https://github.com/tj-actions/changed-files)
+```yaml
+name: With changed files 
+on: [push, pull_request]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+        with:
+          fetch-depth: 0
+
+      - name: Check for any changed files
+        uses: tj-actions/changed-files@v23.1
+        id: changed-files
+        with:
+          files: |
+             *.md
+             **/*.md
+          separator: ";"
+
+      - name: List all changed files
+        run: |
+          for file in ${{ steps.changed-files.outputs.all_changed_files }}; do
+            echo "$file was changed"
+          done
+  
+      - name: Upload docs
+        if: steps.changed-files.outputs.any_changed == 'true'
+        uses: vg-leanix/docs-uploader@master
+        with:
+          docVersion: v1.1.0
+          apiKey: ${{secrets.README_APIKEY}}
+          categoryId: 62b439a44f5a3e003566e740
+          settingsPath: docs.yml
+          changedFiles: ${{ steps.changed-files.outputs.all_changed_files }}
+
 ```
 
 The `settingsPath` yaml should follow this schema:
@@ -44,6 +83,7 @@ docs2:
 | `slug`   | The slug of the specific page    |
 | `categoryId`   | The category id of the specific documentation page    |
 | `settingsPath`   | yaml to point to the individual markdowns to be updated    |
+| `changedFiles`   | If you use this GitHub Action in combination with https://github.com/tj-actions/changed-files then this input will take the changed doc files. Essentially only updating docs that were changed    |
 
 
 
--- main.py
+++ main.py
@@ -10,10 +10,13 @@ def main():
     apikey = os.environ["INPUT_APIKEY"]
     category_id = os.environ["INPUT_CATEGORYID"]
     settings_path = os.environ["INPUT_SETTINGSPATH"]
+    changed_files = os.environ["INPUT_CHANGEDFILES"]
 
     apikey_changed = apikey+":"
     b64_apikey = base64.b64encode(
         apikey_changed.encode("utf-8")).decode("utf-8")
+
+    # if settings path option is used
 
     # getting all the source paths to read markdown
     try:
@@ -26,45 +29,97 @@ def main():
         print(e)
         exit(1)
 
-    print(emoji.emojize(f"Found {len(md.keys())} doc files"))
+    # if changed files are provided only upload changed docs and not all.
+    if changed_files:
+        changed_files_list = changed_files.split(";")
 
-    # updating all docs for named slugs. all assumed to be in same category
-    for integration, value in md.items():
-        print(integration, ": ", value['source'], "| slug: ", value['slug'])
-        path = value['source']
-        slug = value['slug']
+        matched_docs = 0
 
-        try:
-            with open(path, "r") as markdown:
-                integration_doc = markdown.read()
-        except Exception as e:
-            print(e)
+        for integration, value in md.items():
+            if value['source'] in changed_files_list:
+                matched_docs += 1
+                print(f"{integration} was changed.")
+                path = value['source']
+                slug = value['slug']
 
-        url = f"https://dash.readme.com/api/v1/docs/{slug}"
+                try:
+                    with open(path, "r") as markdown:
+                        integration_doc = markdown.read()
+                except Exception as e:
+                    print(e)
 
-        headers = {
-            "Accept": "application/json",
-            "x-readme-version": docversion,
-            "Content-Type": "application/json",
-            "Authorization": f"Basic {b64_apikey}"
-        }
+                url = f"https://dash.readme.com/api/v1/docs/{slug}"
 
-        payload = {
-            "hidden": True,
-            "catgory": category_id,
-            "order": 999,
-            "body": integration_doc
+                headers = {
+                    "Accept": "application/json",
+                    "x-readme-version": docversion,
+                    "Content-Type": "application/json",
+                    "Authorization": f"Basic {b64_apikey}"
+                }
 
-        }
+                payload = {
+                    "hidden": True,
+                    "catgory": category_id,
+                    "order": 999,
+                    "body": integration_doc
 
-        response = requests.put(url, json=payload, headers=headers)
+                }
 
-        if response.status_code != 200:
-            print(response.text)
-            exit(1)
-        elif response.status_code == 200:
-            print(emoji.emojize(
-                f"API docs for {integration} updated successfully! :rocket:"))
+                response = requests.put(url, json=payload, headers=headers)
+
+                if response.status_code != 200:
+                    print(response.text)
+                    exit(1)
+                elif response.status_code == 200:
+                    print(emoji.emojize(
+                        f"Docs for {integration} were updated successfully! :rocket:"))
+
+        if matched_docs == 0:
+            print(
+                f"None of the changed files are docs. Please check your settings file: {settings_path}")
+
+    # otherwise update all docs.
+    else:
+        print(emoji.emojize(f"Found {len(md.keys())} doc files"))
+
+        # updating all docs for named slugs. all assumed to be in same category
+        for integration, value in md.items():
+            print(integration, ": ",
+                  value['source'], "| slug: ", value['slug'])
+            path = value['source']
+            slug = value['slug']
+
+            try:
+                with open(path, "r") as markdown:
+                    integration_doc = markdown.read()
+            except Exception as e:
+                print(e)
+
+            url = f"https://dash.readme.com/api/v1/docs/{slug}"
+
+            headers = {
+                "Accept": "application/json",
+                "x-readme-version": docversion,
+                "Content-Type": "application/json",
+                "Authorization": f"Basic {b64_apikey}"
+            }
+
+            payload = {
+                "hidden": True,
+                "catgory": category_id,
+                "order": 999,
+                "body": integration_doc
+
+            }
+
+            response = requests.put(url, json=payload, headers=headers)
+
+            if response.status_code != 200:
+                print(response.text)
+                exit(1)
+            elif response.status_code == 200:
+                print(emoji.emojize(
+                    f"Docs for {integration} updated successfully! :rocket:"))
 
 
 if __name__ == "__main__":
