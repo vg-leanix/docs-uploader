331ca570432ae95d5de8af78f50ccfab13535f68
Vince <vincent.groves@leanix.net>
2022-06-25 08:41:17 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # reademe-upload
+
+## Usage
+This action helps you automate your changes to your markdown and the subsequent upload to your readme.io project to automate dev workflows.
+
+### Example workflow
+```yaml
+name: My Workflow
+on: [push, pull_request]
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: Run action
+
+      uses: vg-leanix/readme-upload@master
+
+      with:
+        docVersion: v1.0.0
+        apiKey: foobar123
+        slug: test
+        categoryId: 12fff324
+```
+
+### Inputs
+
+| Input                                             | Description                                        |
+|------------------------------------------------------|-----------------------------------------------|
+| `docVersion`  | The document version to upload the documentation for e.g. `v1.0.0`    |
+| `apiKey`   | The API Key from readme.io. See [documentation](https://docs.readme.com/reference/authentication).    |
+
+
+
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import base64
+import requests  # noqa We are just importing this to prove the dependency installed correctly
+
+
+def main():
+    docversion = os.environ["INPUT_DOCVERSION"]
+    apikey = os.environ["INPUT_APIKEY"]
+    slug = os.environ["INPUT_SLUG"]
+    category_id = os.environ["INPUT_CATEGORYID"]
+    markdown_path = os.environ["INPUT_PATH"]
+
+    apikey_changed = apikey+":"
+    b64_apikey = base64.b64encode(apikey_changed.encode("ascii"))
+
+    with open(markdown_path, "r") as md:
+        markdown = md.read()
+
+    url = f"https://dash.readme.com/api/v1/docs/{slug}"
+
+    headers = {
+        "Accept": "application/json",
+        "x-readme-version": docversion,
+        "Content-Type": "application/json",
+        "Authorization": f"Basic {b64_apikey}"
+    }
+
+    payload = {
+        "hidden": True,
+        "catgory":category_id,
+        "order": 999,
+        "body": markdown
+
+    }
+
+    response = requests.put(url, json=payload, headers=headers)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
